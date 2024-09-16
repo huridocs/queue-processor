6f6815b5e1e7bbb3e08682b3159d366d7d8a9e5a
Gabo <gabriel.piles.glez@gmail.com>
2024-09-16 18:59:50 +0200

    Handle unexpected exceptions

--- src/queue_processor/QueueProcessor.py
+++ src/queue_processor/QueueProcessor.py
@@ -1,21 +1,26 @@
 import logging
+from time import sleep
+
+import redis
 from rsmq.cmd import NoMessageInQueue, utils
 from rsmq import RedisSMQ, cmd
-import time
-
-handlers = [logging.StreamHandler()]
-logging.root.handlers = []
-logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s", handlers=handlers)
-queue_processor_logger = logging.getLogger(__name__)
 
 
 class QueueProcessor:
-    def __init__(self, redis_host: str, redis_port: int, queues_names_by_priority: list[str]):
+    def __init__(self, redis_host: str, redis_port: int, queues_names_by_priority: list[str], logger: logging.Logger = None):
         self.redis_host: str = redis_host
         self.redis_port: int = redis_port
         self.task_queues_names: list[str] = [queue_name + "_tasks" for queue_name in queues_names_by_priority]
         self.results_queues_names: list[str] = [queue_name + "_results" for queue_name in queues_names_by_priority]
 
+        if logger:
+            self.queue_processor_logger = logger
+            return
+
+        handlers = [logging.StreamHandler()]
+        logging.root.handlers = []
+        logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s", handlers=handlers)
+        self.queue_processor_logger = logging.getLogger(__name__)
 
     def get_queue(self, queue_name):
         return RedisSMQ(host=self.redis_host, port=self.redis_port, qname=queue_name)
@@ -24,11 +29,11 @@ class QueueProcessor:
         try:
             self.get_queue(queue_name).getQueueAttributes().exec_command()
         except cmd.exceptions.QueueDoesNotExist:
-            queue_processor_logger.info(f"Creating queue")
+            self.queue_processor_logger.info(f"Creating queue")
             self.get_queue(queue_name).createQueue().vt(120).exceptions(False).execute()
 
-    def start(self, process: callable) -> bool:
-        queue_processor_logger.info("Running")
+    def start(self, process: callable):
+        self.queue_processor_logger.info("QueueProcessor running")
         while True:
             try:
                 for queue_name in self.task_queues_names + self.results_queues_names:
@@ -37,17 +42,18 @@ class QueueProcessor:
                 for task_queue_name, results_queue_name in zip(self.task_queues_names, self.results_queues_names):
                     task_queue = self.get_queue(task_queue_name)
                     message = task_queue.receiveMessage().execute()
+                    task_queue.deleteMessage(qname=task_queue_name, id=message["id"]).execute()
 
                     if "message" in message:
                         message["message"] = utils.decode_message(message["message"])
 
-                    task_queue.deleteMessage(qname=task_queue_name, id=message["id"]).execute()
-
                     results = process(message["message"])
                     self.get_queue(results_queue_name).sendMessage().message(results).execute()
-
+            except redis.exceptions.ConnectionError:
+                self.queue_processor_logger.error(f"Error connecting to Redis: {self.redis_host}:{self.redis_port}")
+                sleep(30)
             except NoMessageInQueue:
-                time.sleep(2)
+                sleep(2)
             except Exception as e:
-                queue_processor_logger.error(f"Error: {e}", exc_info=True)
-                return False
+                self.queue_processor_logger.error(f"Error: {e}", exc_info=True)
+                sleep(60)

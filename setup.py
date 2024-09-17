26c9413ac4fd950ace4ee542d6734e6959e10ea4
Gabo <gabriel.piles.glez@gmail.com>
2024-09-17 14:03:55 +0200

    Change version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     name=PROJECT_NAME,
     packages=["queue_processor"],
     package_dir={"": "src"},
-    version="0.3",
+    version="0.4",
     url="https://github.com/huridocs/queue-processor",
     author="HURIDOCS",
     description="Manage queues on Uwazi services",

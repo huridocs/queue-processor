fce76dc0c39a8d929128304eb77047985ccd344a
Gabo <gabriel.piles.glez@gmail.com>
2025-03-14 10:47:42 +0100

    Update version

--- src/check_queues.py
+++ src/check_queues.py
@@ -2,14 +2,17 @@ from time import sleep
 
 from rsmq import RedisSMQ
 
-QUEUES_NAMES = ["production_ocr"]
+QUEUES_NAMES = ["production_extract_paragraphs"]
 
 if __name__ == "__main__":
     for i in range(100):
         for queue_name in QUEUES_NAMES:
             try:
-                print(queue_name)
+                print(f"{queue_name}_tasks")
                 queue = RedisSMQ(host="localhost", qname=f"{queue_name}_tasks")
+                print(queue.getQueueAttributes().exec_command())
+                print(f"{queue_name}_results")
+                queue = RedisSMQ(host="localhost", qname=f"{queue_name}_results")
                 print(queue.getQueueAttributes().exec_command())
             except:
                 pass
